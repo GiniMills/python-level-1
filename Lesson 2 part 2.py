c6f3fabc39326192c33a7da145e729d3b84fc976
GiniMills <117392384+GiniMills@users.noreply.github.com>
2024-05-04 20:28:19 +0100

    Add files via upload

---
+++ Lesson 2 part 2.py
@@ -0,0 +1,18 @@
+#print("Welcome to the quiz on capital cities of the world")
+#while True:
+#  guess=str(input("What is the capital of England?"))
+#  if guess == "London":
+#    print(f"Correct! The capital of England is {guess}!")
+#    break
+#  else:
+#    print(f"Incorrect, the capital of England is not {guess}")
+#    print(f"Try again!")
+
+answer= input("What is the capital city of England?")
+while(answer !="London"):
+  print("Try again")
+  answer= input("What is the capital city of England?")
+print("Well done!")
+
+
+  
